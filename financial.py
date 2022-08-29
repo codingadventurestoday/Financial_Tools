8e5084750553c98f8ff448e0044ab47a55b741cc
codingadventurestoday <97482585+codingadventurestoday@users.noreply.github.com>
2022-08-29 09:24:39 -0700

    Update financial.py
    Added the function for future_purchase

--- financial.py
+++ financial.py
@@ -4,9 +4,53 @@
 3. A process that creates a spending plan based off of an individual's income. 
 4. A simple estimation for yearly taxes. 
 """
+import re
 
 def future_purchase(): 
-    return None 
+    print('let\'s work on a game plan to save up for a future purchase!')
+    check_purchase_amount = True
+    while check_purchase_amount == True: 
+        purchase_amount = input('How much does the future purchase cost?: $')
+        try: 
+            amount_to_purchase = int(purchase_amount)
+            check_purchase_amount = False
+            break
+        except: 
+            print('Please ensure that you enter the mount of the purchase in numerical form')
+    
+    check_decide_path = True 
+    while check_decide_path == True: 
+        decide_path = input(f'''Now that we know your purchase will be ${amount_to_purchase} we can decide how to plan for it.\n
+        Would you like to save by:\n
+        -A fixed amount of savings each month. Please enter fixed.
+            -or-
+        -A timeline. Please enter timeline.\n
+        ''')
+        if re.search('fixed',decide_path.lower()) != None: 
+            #enter fixed monthly savings pathway
+            print('A fixed monthly saving is a great plan for future payments.')
+            check_monthly_savings = True: 
+            while check_monthly_savings == True: 
+                monthly_savings = input('How much would you like to save each month?: $')
+                try: 
+                    monthly_savings = int(monthly_savings)
+                except: 
+                    print('Please enter the amount you will be saving each month is numerical form. Example: 300.\n')
+            amount_of_months_to_save = amount_to_purchase/monthly_savings
+            output_name = f'You will be able to make a purchase of ${amount_to_purchase} in {amount_of_months_to_save} by saving ${monthly_savings} each month.'
+            return output_name
+            check_decide_path = False 
+            break
+            
+        elif re.search('timeline', decide_path.lower()) != None:
+            #enter timeline saving pathway
+            check_decide_path
+            break
+            
+        else:
+            print('Please ensure that you enter either fixed or timeline.\n\n')
+            continue
+    return None  
   
 def previous_purchase_payoff():
     return None 
@@ -36,10 +80,10 @@ current_status = True
 while current_status == True: 
     tool_selection = input('''We have four useful tools you can use. Please select the number of which tool you 
     would like to use:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. Plan for a future purchase
+    2. Plan to pay off a previous purchase
+    3. Create a spending plan
+    4. Estimate the amount of taxes you will pay
     5. To EXIT '''
     try: 
         tool_selection = int(tool_selection)
@@ -49,16 +93,21 @@ while current_status == True:
         
     try:          
         if tool_selection == 1:
-            future_purchase()
+            statement_to_print = future_purchase()
+            print(f'{statement_to_print}')
+            return_to_homepage()
                      
         elif tool_selection == 2: 
-            previous_purchase_payoff()               
+            previous_purchase_payoff()  
+            return_to_homepage()
                            
         elif tool_selection == 3: 
-            spending_plan()               
+            spending_plan()     
+            return_to_homepage()
               
         elif tool_selection == 4:                   
             estimated_taxes()  
+            return_to_homepage()
                            
         elif tool_selection == 5: 
             current_status = False 
